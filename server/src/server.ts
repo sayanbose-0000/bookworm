54c92902e28a01b9d4bbb89f34dfbfa6640e2685
sayanbose-0000 <bosesayan0000@gmail.com>
2026-04-08 07:20:10 +0530

    Book service started

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,37 @@
 import authApp from '@/controller/user/AuthController';
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 import mongoose from 'mongoose';
+
 
 const server = new Hono();
 
-mongoose.connect(Bun.env.MONGO_URI as string)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch(err => {
-    console.log("Failed to connect to mongodb", err);
-  });
+server.onError((err, c) => {
+  // HTTPException Errors
+  if (err instanceof HTTPException) {
+    return c.json({
+      message: err.message
+    }, err.status);
+  }
+
+  // Generic errors
+  return c.json({
+    message: "Internal Server Error"
+  }, 500);
+
+});
+
+mongoose.connect(Bun.env.MONGO_URI as string).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err => {
+  console.log("Failed to connect to mongodb", err);
+});
 
 server.get("/", (c) => {
   return c.json({ message: "Welcome to Bookworm" });
 });
+
+
 server.route("/auth", authApp);
 // app.route('/books');
 // app.route("/annotation")
--- server/src/services/book/BookService.ts
+++ server/src/services/book/BookService.ts
@@ -1,10 +1,15 @@
 import { validateInputWithZod } from "@/helper/validation/validateInput";
 import { BookCreateZodSchema, IBookCreateInput } from "@/validations/book/BookZod";
+import { fetch } from "bun";
 
 const createBook = async (data: IBookCreateInput) => {
   const { file } = await validateInputWithZod(BookCreateZodSchema, data);
   
-  
+  await fetch(`${Bun.env.FAST_IO_API_KEY}`, {
+    headers: {
+      "Authorization": "Bearer ",
+    }
+  });
 };
 
 export { createBook };
